6e233eea32ee48ac7ac6271ec8cb3c689d5de734
it21352430 <99311002+it21352430@users.noreply.github.com>
2022-02-09 06:28:47 +0000

    I have completed 2nd question

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,21 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int distance,amount;
+
+  printf("enter the distance van has travelled :");
+  scanf("%d",&distance);
+
+  if(distance<=30)
+  {
+    amount=distance*50;
+  }
+  else if(distance>30)
+  {
+    amount=30*50+(distance-30)*40;
+  }
+
+  printf("amount to be paid :%d",amount);
   
   return 0;
 }
